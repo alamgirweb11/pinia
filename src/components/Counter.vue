c2158be8430c98a6c8fcba5a27ba8070e983ac01
alamgirweb11 <alamgirweb11@gmail.com>
2023-04-18 23:56:28 +0600

    counter component added

---
+++ src/components/Counter.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+const store = useCounterStore()
+const counter = store.count
+</script>
+<template>
+    <h5>Counter Value: {{ counter }}</h5>
+</template>
