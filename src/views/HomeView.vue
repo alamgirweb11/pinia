dd516758cbd70ae56ded0ec8d7175812a87cfd83
alamgirweb11 <alamgirweb11@gmail.com>
2023-04-19 23:41:08 +0600

    modify counter component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,8 @@
 <script setup>
-import { useCounterStore } from '@/stores/counter'
-const store = useCounterStore()
-const counter = store.count
+import CounterComponent from '@/components/CounterComponent.vue'
 </script>
 <template>
   <div class="home">
-    <h4>This is home page.</h4>
-    <h5>Counter Value: {{ counter }}</h5>
+   <CounterComponent />
   </div>
 </template>
