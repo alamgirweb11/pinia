4abf7f30c8311024a4b61c1f005d6cb6d1876966
alamgirweb11 <alamgirweb11@gmail.com>
2023-04-25 23:59:36 +0600

    increment decrement btn added

--- src/components/CounterComponent.vue
+++ src/components/CounterComponent.vue
@@ -2,7 +2,16 @@
 import { useCounterStore } from '@/stores/counter'
 const store = useCounterStore()
 const counter = store.count
+function increment () {
+  counter.value++
+}
+function decrement () {
+  counter.value--
+}
 </script>
 <template>
+    <button @click="increment">+</button>
     <h5>Counter Value: {{ counter }}</h5>
+    <h6>Digit Length {{ store.countDigitLength }}</h6>
+    <button @click="decrement">-</button>
 </template>
