c7d6a96f0b0469986fa74e40e0173947ed6b97b9
alamgirweb11 <alamgirweb11@gmail.com>
2023-04-27 23:57:58 +0600

    basic uses of storeToRefs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <script setup>
+import { storeToRefs } from 'pinia'
 import CounterComponent from '@/components/CounterComponent.vue'
 import { useAuthUser } from '@/stores/auth'
 const authStore = useAuthUser()
-
+const { isAuthenticate, user } = storeToRefs(authStore)
 </script>
 <template>
   <div class="home">
-    <div v-if="authStore.isAuthenticate">User Name : {{ authStore.user.name }}</div>
+    <div v-if="isAuthenticate">User Name : {{ user.name }}</div>
    <CounterComponent />
   </div>
 </template>
