cf06028d354489761814ee0b442387bc29e6daad
alamgirweb11 <alamgirweb11@gmail.com>
2023-05-01 12:29:47 +0600

    use asyc await in actions

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -2,19 +2,22 @@ import { defineStore } from 'pinia'
 
 export const useAuthUser = defineStore('authUser', {
   state: () => ({
-    isAuthenticate: true,
-    user: {
-      name: 'Abir Ahmed',
-      email: 'abir@gmail.com'
-    }
+    isAuthenticate: false,
+    user: {}
   }),
+  getters: {
+    fullName: (state) => `${state.user.first_name} ${state.user.last_name}`
+  },
   actions: {
     logout () {
       this.isAuthenticate = false
     },
 
-    login () {
-      this.$reset()
+    async login () {
+      const res = await fetch('https://reqres.in/api/users/2')
+      const { data } = await res.json()
+      this.user = data
+      this.isAuthenticate = true
     }
   }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,11 @@ import { storeToRefs } from 'pinia'
 import CounterComponent from '@/components/CounterComponent.vue'
 import { useAuthUser } from '@/stores/auth'
 const authStore = useAuthUser()
-const { isAuthenticate, user } = storeToRefs(authStore)
+const { isAuthenticate, fullName } = storeToRefs(authStore)
 </script>
 <template>
   <div class="home">
-    <div v-if="isAuthenticate">User Name : {{ user.name }}</div>
+    <div v-if="isAuthenticate">User Name : {{ fullName }}</div>
    <CounterComponent />
   </div>
 </template>
